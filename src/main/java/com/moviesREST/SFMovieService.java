2d1d17552f0e0dde9d1e56dba16aa70d06848762
Seek Addo <dennis_addo@aol.com>
2018-07-08 09:30:39 +0200

    Git commit to trigger travis build and test code on server

---
+++ src/main/java/com/moviesREST/SFMovieService.java
@@ -0,0 +1,88 @@
+package com.moviesREST;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ *  Created by IntelliJ IDEA.
+ *   User: addodennis
+ *   Date: 8/7/18
+ *   Time: 5:17 PM
+ *   To change this template use File | Settings | File Templates.
+ */
+
+
+/**
+ * Root resource (exposed at "sfmservice" path)
+ */
+
+@Path("sfmservice")
+public class SFMovieService {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/json" media type.
+     *
+     * @return http response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response GetAllMovies() {
+        String url = "https://data.sfgov.org/resource/wwmu-gmzc.json";
+
+        return remoteDataJSON(url);
+
+    }
+
+    /**
+     * Service with filters (Filmname)
+     *
+     * @return http response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{filmname}")
+    public Response GetMoviesByFilter(@PathParam("filmname") String filmname) {
+
+        String url;
+        if (filmname != null) {
+            url = "https://data.sfgov.org/resource/wwmu-gmzc.json?title=" + filmname.replaceAll(" ", "%20");
+        } else {
+            return Response.status(500).entity("{response: \"Empty filter params\"}").build();
+        }
+
+        System.out.println(url);
+
+        return remoteDataJSON(url);
+
+    }
+
+    /**
+     * Extracting remote data with one function to prevent code duplications.
+     */
+    private static Response remoteDataJSON(String url) {
+        StringBuilder jsonText = new StringBuilder();
+
+        try (BufferedReader in = new
+                BufferedReader(new InputStreamReader((new URL(url)).openConnection().getInputStream()))) {
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                jsonText.append(line);
+            }
+            //jsonText = in.lines().collect(Collectors.joining(""));
+
+            return Response.status(200).type("application/json").entity(jsonText.toString()).build();
+        } catch (IOException e) {
+            return Response.status(500).type("application/json").entity("Server Error: " + e.getMessage()).build();
+        }
+
+    }
+}
---
+++ src/test/java/com/moviesREST/SFMovieServiceTest.java
@@ -0,0 +1,53 @@
+package com.moviesREST;
+
+import javax.validation.constraints.AssertTrue;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class SFMovieServiceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(SFMovieService.class);
+    }
+
+    /**
+     * Test to see that the movies result empty in the response.
+     */
+    @Test
+    public void testGetMovies() {
+        final String responseMsg = target().path("sfmservice").request().get(String.class);
+
+        assertNotEquals(0,responseMsg.length());
+    }
+
+    /**
+     * Test to see that the requested movie with title 180 returned something in the response.
+     */
+    @Test
+    public void testGetMoviesByTitle() {
+        final Response responseMsg = target().path("sfmservice/180").request().get();
+
+        assertEquals("should return status 200", 200, responseMsg.getStatus());
+    }
+
+    /**
+     * Test to see without pagenot found response works.
+     */
+    @Test
+    public void testPageNotFound() {
+        final Response responseMsg = target().path("sfmservic34").request().get();
+
+        assertEquals("should return status 404", 404, responseMsg.getStatus());
+    }
+}
