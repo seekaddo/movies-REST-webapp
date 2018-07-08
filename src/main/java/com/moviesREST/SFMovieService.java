ce23f37d6a3024b09569ecd5645d37cdc6b3a55b
Seek Addo <dennis_addo@aol.com>
2018-07-08 10:06:35 +0200

    removed unused imports

--- src/main/java/com/moviesREST/SFMovieService.java
+++ src/main/java/com/moviesREST/SFMovieService.java
@@ -7,8 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.List;
-import java.util.stream.Collectors;
+
 
 /*
  *  Created by IntelliJ IDEA.
