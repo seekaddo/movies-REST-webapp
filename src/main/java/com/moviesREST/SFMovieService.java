a4d591dec5bb020d7eaa7ed1db43637aba3f3f89
Addo Dennis <dennis_addo@aol.com>
2018-07-18 00:28:32 +0200

    updated with the right attribute filters

--- src/main/java/com/moviesREST/SFMovieService.java
+++ src/main/java/com/moviesREST/SFMovieService.java
@@ -1,12 +1,21 @@
 package com.moviesREST;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 /*
@@ -22,7 +31,7 @@ import java.net.URL;
  * Root resource (exposed at "sfmservice" path)
  */
 
-@Path("sfmservice")
+@Path("/sfmservice")
 public class SFMovieService {
 
     /**
@@ -41,23 +50,32 @@ public class SFMovieService {
     }
 
     /**
-     * Service with filters (Filmname)
+     * Service with filters (title) and (location)
+     *
+     *
+     * All queries are handled by this function.
      *
      * @return http response.
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{filmname}")
-    public Response GetMoviesByFilter(@PathParam("filmname") String filmname) {
+    @Path("/query")
+    public Response GetMoviesByFilter(@Context UriInfo params) {
 
+        String title = params.getQueryParameters().getFirst("title");
+        String location = params.getQueryParameters().getFirst("location");
         String url;
-        if (filmname != null) {
-            url = "https://data.sfgov.org/resource/wwmu-gmzc.json?title=" + filmname.replaceAll(" ", "%20");
-        } else {
-            return Response.status(500).entity("{response: \"Empty filter params\"}").build();
-        }
 
-        System.out.println(url);
+        System.out.println("title: "+title);
+        System.out.println("location: "+location);
+        if(title != null && location == null){
+            url = "https://data.sfgov.org/resource/wwmu-gmzc.json?title=" + title.replaceAll(" ", "%20");
+        }else if(location != null) {
+            url = "https://data.sfgov.org/resource/wwmu-gmzc.json?locations=" + location.replaceAll(" ", "%20");
+        }else {
+            //Both params null, panic to the user with empty query response.
+            return  Response.ok().type("application/json").entity("Server Error: Empty query").build();
+        }
 
         return remoteDataJSON(url);
 
@@ -65,10 +83,17 @@ public class SFMovieService {
 
     /**
      * Extracting remote data with one function to prevent code duplications.
+     * All necessary data will be extracted from the returned json response from remote server
+     * title and location is information here.
      */
     private static Response remoteDataJSON(String url) {
-        StringBuilder jsonText = new StringBuilder();
 
+        System.out.println("Paseed: "+url);
+        StringBuilder jsonText = new StringBuilder();
+        String jsonstring;
+
+        //This is needed to help order the Json elements. because JSONObject is a unorder map.
+        HashMap<String,String> objt = new LinkedHashMap<>();
         try (BufferedReader in = new
                 BufferedReader(new InputStreamReader((new URL(url)).openConnection().getInputStream()))) {
 
@@ -76,11 +101,30 @@ public class SFMovieService {
             while ((line = in.readLine()) != null) {
                 jsonText.append(line);
             }
-            //jsonText = in.lines().collect(Collectors.joining(""));
 
-            return Response.status(200).type("application/json").entity(jsonText.toString()).build();
+
+            JSONArray list = new JSONArray();
+
+            JSONArray jsonArray = new JSONArray(jsonText.toString());
+            System.out.println("Size is: "+jsonArray.length());
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                //optString:  Don't break if elements not found, just return empty string
+                String location = jsonArray.getJSONObject(i).optString("locations");
+                String title = jsonArray.getJSONObject(i).optString("title");
+
+                if(!location.isEmpty() && !title.isEmpty()){
+                    objt.put("locations",location);
+                    objt.put("title",title);
+                    list.put(objt);
+                }
+
+            }
+            jsonstring = Stream.of(list).map(JSONArray::toString).collect(Collectors.joining(","));
+
+            return Response.status(200).type("application/json").entity(jsonstring).build();
         } catch (IOException e) {
-            return Response.status(500).type("application/json").entity("Server Error: " + e.getMessage()).build();
+            return Response.ok().type("application/json").entity("Server Error: " + e.getMessage()).build();
         }
 
     }
---
+++ target/movies-REST-webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: addodennis
+  Date: 8/7/18
+  Time: 8:17 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Movie Rest Service$</title>
+  </head>
+  <body>
+  Welcome to the Movies night  in San Francisco
+  <p><a href="api_v1.0/sfmservice">SF Movies</a>
+  </body>
+</html>
